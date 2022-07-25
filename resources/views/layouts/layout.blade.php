d99445e03085b5c72622dba2a6f3ca99b7420f1c
wiratama mukti <wrtmukti@gmail.com>
2022-07-25 08:29:30 +0700

    update viewport

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -3,8 +3,7 @@
 
 <head>
     <meta charset="utf-8" />
-    {{-- <meta http-equiv="x-ua-compatible" content="ie=edge" /> --}}
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge" />
 
     <title>KEDAI PAPAJI</title>
     <meta name="description" content="" />
