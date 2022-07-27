87927f0294aea12e083efff43312f57aee1a1772
wiratama mukti <wrtmukti@gmail.com>
2022-07-27 23:23:07 +0700

    validate no_table

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -168,7 +168,7 @@
             <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="exampleModalLabel">Keranjang Saya</h5>
-                <span class="text-danger">*pastikan order sebelumnya tidak tercantum</span>
+                <p class="text-danger">*pastikan order sebelumnya tidak tercantum</p>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <form action="/order/store" method="post">
