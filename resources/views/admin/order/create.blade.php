5c27879eb0d7997d571b6c0817ece8f69f0d089e
wiratama mukti <wrtmukti@gmail.com>
2022-07-27 19:57:16 +0700

    fix tabs

--- resources/views/admin/order/create.blade.php
+++ resources/views/admin/order/create.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 
 {{-- cart icon --}}
-<div class="row ">
+<div class="row " style="position: sticky; top: 0; z-index: 100;">
   <div class="col-5 mb-3">
     <!-- Button trigger modal -->
     <div class="shopping-cart">
