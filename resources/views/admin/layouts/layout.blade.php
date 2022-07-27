ea4440a801240488af475c57cce156b1e15b51a0
wiratama mukti <wrtmukti@gmail.com>
2022-07-27 19:43:16 +0700

    fix tabs

--- resources/views/admin/layouts/layout.blade.php
+++ resources/views/admin/layouts/layout.blade.php
@@ -26,6 +26,7 @@
   <link rel="stylesheet" href="{{ asset('vendor/star-admin/template/js/select.dataTables.min.css') }}">
   <link rel="stylesheet" href="{{ asset('css/admin.css') }}">
   <link rel="stylesheet" href="{{ asset('css/tabslider.css') }}">
+  
   <!-- End plugin css for this page -->
   <!-- inject:css -->
   <link rel="stylesheet" href="{{ asset('vendor/star-admin/template/css/vertical-layout-light/style.css') }}">
@@ -289,6 +290,7 @@
   <!-- container-scroller -->
  {{-- plugin cart --}}
  <script src="{{ asset('js/tabslider.js') }}"></script>
+ <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
 
   <!-- plugins:js -->
   <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
