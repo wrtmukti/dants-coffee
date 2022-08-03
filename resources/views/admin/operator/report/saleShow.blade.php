2c50bc77dc7c23a590f91d395c9b62cc11a3ed19
wiratama mukti <wrtmukti@gmail.com>
2022-08-03 07:31:54 +0700

    fix date

--- resources/views/admin/operator/report/saleShow.blade.php
+++ resources/views/admin/operator/report/saleShow.blade.php
@@ -49,7 +49,7 @@
                     </td>
                     <td class="text-center">
                      <?php $quantity = 0  ?>
-                     @foreach ($data->orders as $item)
+                     @foreach ($data->orders->whereRelation('orders', DB::raw("(DATE_FORMAT(created_at,'%m-%Y'))"), '=', $date) as $item)
                          <?php $quantity += $item->pivot->quantity ?> 
                      @endforeach
                       <a   class="nav-link  text-dark">{{ $quantity }} item Terjual</a>
